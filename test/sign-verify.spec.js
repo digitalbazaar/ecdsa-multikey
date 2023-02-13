85e4feb0a6f3f1d3f75f3919556dc0cdfdcb174d
Kayode Ezike <kezike13@gmail.com>
2023-02-13 08:36:03 -0500

    added ecdsa sign and verify tests

--- test/sign-verify.spec.js
+++ test/sign-verify.spec.js
@@ -1,13 +1,12 @@
 /*!
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
+import * as base58 from 'base58-universal';
 import chai from 'chai';
-chai.should();
-
 import * as EcdsaMultikey from '../lib/index.js';
 import {mockKey, suites} from './mock-data.js';
 import {stringToUint8Array} from './text-encoder.js';
-import * as base58btc from 'base58-universal';
+chai.should();
 
 const keyPair = await EcdsaMultikey.from({
   controller: 'did:example:1234',
@@ -18,15 +17,37 @@ const verifier = keyPair.verifier();
 
 describe('sign and verify', () => {
   it('works properly', async () => {
+    signer.should.have.property('id',
+      'did:example:1234#zynkLvFajqEANBYZ7BbjYfjZWEKxC2o1cFWbvsK4XzSDyjJ6Unze3XNAvBNKkfCPRHAEQY');
+    verifier.should.have.property('id',
+      'did:example:1234#zynkLvFajqEANBYZ7BbjYfjZWEKxC2o1cFWbvsK4XzSDyjJ6Unze3XNAvBNKkfCPRHAEQY');
+    const data = stringToUint8Array('test 1234');
+    const signature = await signer.sign({data});
+    const result = await verifier.verify({data, signature});
+    result.should.be.true;
   });
 
   it('fails if signing data is changed', async () => {
+    const data = stringToUint8Array('test 1234');
+    const signature = await signer.sign({data});
+    const changedData = stringToUint8Array('test 4321');
+    const result = await verifier.verify({data: changedData, signature});
+    result.should.be.false;
   });
   // these tests simulate what happens when a key & signature
   // created in either the browser or the node is verified
   // in a different enviroment
   for(const suite of suites) {
     it(suite.title, async () => {
+      const _keyPair = await EcdsaMultikey.from({
+        controller: 'did:example:1234',
+        ...suite.key
+      });
+
+      const data = stringToUint8Array(suite.data);
+      const signature = base58.decode(suite.signature);
+      const result = await _keyPair.verifier().verify({data, signature});
+      result.should.be.true;
     });
   }
 });
