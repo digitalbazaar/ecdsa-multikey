a64475b9ece7efadd9d99c06a522b6b7a1c8b000
Kayode Ezike <kezike13@gmail.com>
2023-02-13 07:50:43 -0500

    fixed error message of stringToUint8Array function

--- test/text-encoder.js
+++ test/text-encoder.js
@@ -7,7 +7,7 @@ export function stringToUint8Array(data) {
     return new TextEncoder().encode(data);
   }
   if(!(data instanceof Uint8Array)) {
-    throw new TypeError('"data" be a string or Uint8Array.');
+    throw new TypeError('"data" must be a string or Uint8Array.');
   }
   return data;
 }
