52c47036a5662ad4b432b217b0023980fceabe7b
Kayode Ezike <kezike13@gmail.com>
2023-02-21 18:51:46 -0500

    improved code coverage

--- test/mock-data.js
+++ test/mock-data.js
@@ -7,3 +7,24 @@ export const mockKey = {
   publicKeyMultibase: 'zDnaeSMnptAKpH4AD41vTkwzjznW7yNetdRh9FJn8bJsbsdbw',
   secretKeyMultibase: 'z42twirSb1PULt5Sg6gjgNMsdiLycu6fbA83aX1vVb8e3ncP'
 };
+
+export const mockEcdsaSecp256r1VerificationKey2019 = {
+  type: 'EcdsaSecp256r1VerificationKey2019',
+  controller: 'did:example:1234',
+  publicKeyMultibase: 'zDnaeSMnptAKpH4AD41vTkwzjznW7yNetdRh9FJn8bJsbsdbw',
+  secretKeyMultibase: 'z42twirSb1PULt5Sg6gjgNMsdiLycu6fbA83aX1vVb8e3ncP'
+};
+
+export const mockEcdsaSecp384r1VerificationKey2019 = {
+  type: 'EcdsaSecp384r1VerificationKey2019',
+  controller: 'did:example:1234',
+  publicKeyMultibase: 'zDnaeSMnptAKpH4AD41vTkwzjznW7yNetdRh9FJn8bJsbsdbw',
+  secretKeyMultibase: 'z42twirSb1PULt5Sg6gjgNMsdiLycu6fbA83aX1vVb8e3ncP'
+};
+
+export const mockEcdsaSecp521r1VerificationKey2019 = {
+  type: 'EcdsaSecp521r1VerificationKey2019',
+  controller: 'did:example:1234',
+  publicKeyMultibase: 'zDnaeSMnptAKpH4AD41vTkwzjznW7yNetdRh9FJn8bJsbsdbw',
+  secretKeyMultibase: 'z42twirSb1PULt5Sg6gjgNMsdiLycu6fbA83aX1vVb8e3ncP'
+};
