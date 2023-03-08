315d71f02819d5438bcd46ef96e02c2588f5640f
Kayode Ezike <kezike13@gmail.com>
2023-03-08 13:55:48 -0500

    Add tests for new signature data format.

--- test/sign-verify.spec.js
+++ test/sign-verify.spec.js
@@ -6,6 +6,7 @@ import * as EcdsaMultikey from '../lib/index.js';
 import {mockKey} from './mock-data.js';
 import {stringToUint8Array} from './text-encoder.js';
 chai.should();
+const {expect} = chai;
 
 const keyPair = await EcdsaMultikey.from({
   controller: 'did:example:1234',
@@ -28,6 +29,24 @@ describe('sign and verify', () => {
     const signature = await signer.sign({data});
     const result = await verifier.verify({data, signature});
     result.should.be.true;
+  });
+
+  it('has proper signature format', async () => {
+    const data = stringToUint8Array('test 1234');
+    const signature = await signer.sign({data});
+
+    // check some expected properties of a Buffer object
+    expect(signature).to.have.property('buffer');
+    expect(signature).to.have.property('byteLength');
+    expect(signature).to.have.property('byteOffset');
+    expect(signature).to.have.property('compare');
+    expect(signature).to.have.property('copy');
+    expect(signature).to.have.property('entries');
+    expect(signature).to.have.property('equals');
+    expect(signature).to.have.property('fill');
+    expect(signature).to.have.property('includes');
+    expect(signature).to.have.property('slice');
+    expect(signature).to.have.property('write');
   });
 
   it('fails if signing data is changed', async () => {
