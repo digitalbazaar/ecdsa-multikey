cff9c9a595e6e88031125cd5351512d8ffef0c75
Andrew Jones <ajones@digitalbazaar.com>
2024-07-05 16:01:13 +0000

    Assert on id & keyType in sign & verify.

--- test/sign-verify.spec.js
+++ test/sign-verify.spec.js
@@ -9,43 +9,52 @@ import {stringToUint8Array} from './text-encoder.js';
 chai.should();
 const {expect} = chai;
 
-for(const [keyType, exportedKey] of keyTypes) {
-  _testKeyType({keyType, exportedKey});
-}
-
-function _testKeyType({keyType, exportedKey}) {
-  describe(keyType, function() {
-    describe(`sign and verify`, async function() {
-      const id = `${exportedKey.controller}#${exportedKey.publicMultibase}`;
-      const keyPair = await EcdsaMultikey.from({
-        id,
-        ...exportedKey
-      });
-      const signer = keyPair.signer();
-      const verifier = keyPair.verifier();
-
-      it('works properly', async () => {
-        signer.should.have.property('id', id);
-        verifier.should.have.property('id', id);
-        const data = stringToUint8Array('test 1234');
-        const signature = await signer.sign({data});
-        const result = await verifier.verify({data, signature});
-        result.should.be.true;
-      });
-
-      it('has proper signature format', async () => {
-        const data = stringToUint8Array('test 1234');
-        const signature = await signer.sign({data});
-        expect(signature).to.be.instanceof(Uint8Array);
-      });
-
-      it('fails if signing data is changed', async () => {
-        const data = stringToUint8Array('test 1234');
-        const signature = await signer.sign({data});
-        const changedData = stringToUint8Array('test 4321');
-        const result = await verifier.verify({data: changedData, signature});
-        result.should.be.false;
-      });
+describe('sign and verify', function() {
+  for(const [keyType, exportedKey] of keyTypes) {
+    describe(keyType, function() {
+      const id = `${exportedKey.controller}#${exportedKey.publicKeyMultibase}`;
+      _testKeyType({id, exportedKey, keyType});
     });
+  }
+});
+
+function _testKeyType({id, exportedKey, keyType}) {
+  let signer;
+  let verifier;
+  before(async function() {
+    const keyPair = await EcdsaMultikey.from({
+      id,
+      ...exportedKey
+    });
+    signer = keyPair.signer();
+    verifier = keyPair.verifier();
+  });
+  it('should have correct id', function() {
+    signer.should.have.property('id', id);
+    verifier.should.have.property('id', id);
+  });
+  it(`should have algorithm ${keyType}`, function() {
+    signer.should.have.property('algorithm', keyType);
+    verifier.should.have.property('algorithm', keyType);
+  });
+  it('should sign & verify', async function() {
+    const data = stringToUint8Array('test 1234');
+    const signature = await signer.sign({data});
+    const result = await verifier.verify({data, signature});
+    result.should.be.true;
+  });
+
+  it('has proper signature format', async function() {
+    const data = stringToUint8Array('test 1234');
+    const signature = await signer.sign({data});
+    expect(signature).to.be.instanceof(Uint8Array);
+  });
+
+  it('fails if signing data is changed', async function() {
+    const data = stringToUint8Array('test 1234');
+    const signature = await signer.sign({data});
+    const changedData = stringToUint8Array('test 4321');
+    const result = await verifier.verify({data: changedData, signature});
+    result.should.be.false;
   });
 }
